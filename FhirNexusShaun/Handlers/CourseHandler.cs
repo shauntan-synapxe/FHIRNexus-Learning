e22a66d4f8334aa34d6114a69ce04274b71aefc7
Shaun Tan Mun Weng (Synapxe) <shaun.tan1@synapxe.sg>
2025-02-18 16:40:54 +0800

    enroll validation

--- FhirNexusShaun/Handlers/CourseHandler.cs
+++ FhirNexusShaun/Handlers/CourseHandler.cs
@@ -6,6 +6,7 @@ using Ihis.FhirEngine.Core.Data;
 using Ihis.FhirEngine.Core.Exceptions;
 using Ihis.FhirEngine.Core.Models;
 using Ihis.FhirEngine.Core.Search;
+using Newtonsoft.Json;
 using System.Net;
 using Task = System.Threading.Tasks.Task;
 
@@ -26,9 +27,10 @@ namespace FhirNexusShaun.Handlers
             Console.WriteLine("---------------------------------------------");
 
             // debug, write the input into log.
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(input);
-            Console.WriteLine(json);
+            string inputJson = JsonConvert.SerializeObject(input);
+            Console.WriteLine(inputJson);
 
+            //* ---------------------------------------------------------------
             //* Check that duplicate course (same title and start date) exists.
             SearchResult dupCourseResults = await searchService.SearchAsync(nameof(Course), [
                 ("title", input.Title),
@@ -40,6 +42,7 @@ namespace FhirNexusShaun.Handlers
                 throw new PreconditionFailedException("Course with the same title and start date already exists.");
             }
 
+            //* ---------------------------------------------------------------
             //* Course must be at least 1 month later than the created date.
             DateTime startDate = DateTime.Parse(input.StartDate);
             if (startDate < DateTime.Now.AddMonths(1))
@@ -64,16 +67,49 @@ namespace FhirNexusShaun.Handlers
             Console.WriteLine("PreEnroll Start");
             Console.WriteLine("---------------------------------------------");
 
-            Course course = await dataService.GetAsync<Course>(resourceKey, cancellationToken)
+            // debug
+            Console.WriteLine("Resource Key = [{0}]", resourceKey);
+
+            // debug
+            string traineeJson = JsonConvert.SerializeObject(trainee);
+            Console.WriteLine("Trainee = {0}", traineeJson);
+
+            Course selectedCourse = await dataService.GetAsync<Course>(resourceKey, cancellationToken)
                 ?? throw new ResourceNotFoundException(resourceKey);
 
-            // Check if the trainee is already enrolled.
-            // Check if the trainee enrolled in another overlapping course.
+            // debug
+            string courseJson = JsonConvert.SerializeObject(selectedCourse);
+            Console.WriteLine("Selected Course = {0}", courseJson);
 
-            // debug, write the course into log.
-            string json = Newtonsoft.Json.JsonConvert.SerializeObject(trainee);
-            Console.WriteLine(json);
-            Console.WriteLine(resourceKey);
+            //* ---------------------------------------------------------------
+            string inputNric = trainee.Identifier.Value;
+
+            //* ---------------------------------------------------------------
+            // Check if the trainee is already enrolled.
+            bool isEnrolled = selectedCourse.Trainees.Any(
+                enrolledTrainee => enrolledTrainee.Identifier.Value == inputNric
+            );
+
+            if (isEnrolled == true)
+            {
+                throw new PreconditionFailedException("Trainee is already enrolled in this course.");
+            }
+
+            //* ---------------------------------------------------------------
+            //* Check if the trainee enrolled in another overlapping course.
+            string todayStr = DateTime.Now.ToString("yyyy-MM-dd");
+
+            SearchResult overlappingCourse = await searchService.SearchAsync(nameof(Course), [
+                ("trainee", inputNric),
+                ("startDate", "ge" + todayStr),
+                ("endDate", "le" + todayStr)
+            ], false, cancellationToken);
+
+            if (overlappingCourse.Results.Any())
+            {
+                string overlappingCourseId = overlappingCourse.Results.First().Resource.ToResource().Id;
+                throw new PreconditionFailedException("Trainee is enrolled in another overlapping course (Course ID " + overlappingCourseId + ")");
+            }
 
             Console.WriteLine("---------------------------------------------");
             Console.WriteLine("PreEnroll End");
